593a9326b2643f7a9d0b03fed0a3a02aa21d3d19
oscarmrt <oscarmrt@hotmail.com>
2019-09-19 21:18:23 +0000

    modified file 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+/* This program will assign a random number*/
 int main(void)
 {
 int n;
