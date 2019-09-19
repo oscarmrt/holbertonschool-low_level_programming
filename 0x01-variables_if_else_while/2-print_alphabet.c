e4e3b9d0e61731b4667b28d11fb85d4ac67bf2dd
oscarmrt <oscarmrt@hotmail.com>
2019-09-19 22:37:59 +0000

    file 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int alp = 97;
+while (alp < 123)
+{
+putchar(alp);
+alp++;
+}
+putchar('\n');
+return (0);
+}
