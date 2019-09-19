3fea21157ba0a49f69d651fbe78f01916a3ba152
oscarmrt <oscarmrt@hotmail.com>
2019-09-19 23:19:23 +0000

    file 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n = 0;
+while (n <= 9)
+{
+printf("%d", n);
+n++;
+}
+printf("\n");
+return (0);
+}
