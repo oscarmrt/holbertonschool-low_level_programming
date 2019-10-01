30728f80c34eb2ac3d03adb62d2e1d362544557e
oscarmrt <oscarmrt@hotmail.com>
2019-10-01 15:09:48 +0000

    file 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *  reset_to_98 - Entry point
+ *
+ * Description: write a function that takes a pointer to an int as parameter
+ * @n: Is the parameter to be checked
+ * Return: Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
