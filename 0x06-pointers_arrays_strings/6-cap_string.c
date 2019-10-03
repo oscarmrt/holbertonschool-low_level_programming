3110d131821d51c373fdee6e8e30518e90c9324d
oscarmrt <oscarmrt@hotmail.com>
2019-10-03 23:08:43 +0000

    modified file 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,11 +10,24 @@
 char *cap_string(char *cap)
 {
 	int h;
+	char sep[] = {10, 9, 32, 44, 46, 59, 33, 63, 34, 40, 41, 123, 125};
+	int j;
 
 	for (h = 0; cap[h] != '\0'; h++)
 	{
-		if (cap[h - 1] == ' ' && cap[h] >= 97 && cap[h] <= 122)
-			cap[h] = cap[h] - 32;
+		if (cap[h] >= 97 && cap[h] <= 122)
+		{
+			if (h == 0)
+			cap[0] = cap[0] - 32;
+			else
+			{
+				for (j = 0; sep[j] != '\0'; j++)
+				{
+					if (cap[h - 1] == sep[j])
+						cap[h] = cap[h] - 32;
+				}
+			}
+		}
 	}
 	return (cap);
 }
