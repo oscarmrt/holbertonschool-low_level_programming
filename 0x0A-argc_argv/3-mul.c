1301e4cc629393918a3fc7cec61e3d58f41f7226
oscarmrt <oscarmrt@hotmail.com>
2019-10-14 01:48:49 +0000

    file 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+/**
+ *  main - Entry point
+ *
+ * Description: write a program that multiplies two numbers
+ * @argc: Is the parameter to be checked
+ * @argv: Is the parameter to be checked
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int k;
+	int q;
+	int r;
+
+	if ((argc - 1) != 2)
+	{
+	printf("error\n");
+	return (1);
+	}
+	k = atoi(argv[1]);
+	q = atoi(argv[2]);
+	r = k * q;
+	printf("%d\n", r);
+	return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c~
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ *  main - Entry point
+ *
+ * Description: write a program that prints all arguments it receives
+ * @argc: Is the parameter to be checked
+ * @argv: Is the parameter to be checked
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int k = 0;
+
+	while (k < argc)
+	{
+	printf("%s\n", argv[k]);
+	k++;
+	}
+	return (0);
+}
