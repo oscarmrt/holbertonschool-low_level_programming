4ddc4c28455e214f787cde92bf727a7a8e5fb05d
oscarmrt <oscarmrt@hotmail.com>
2019-10-14 00:14:31 +0000

    modified file 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,15 @@
-#include,stdio.h>
-
-int main (int argc, char *argv{})
+#include<stdio.h>
+/**
+ *  main - Entry point
+ *
+ * Description: write a program that prints its name
+ * @argc: Is the parameter to be checked
+ * @argv: Is the parameter to be checked
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv{0});
+	printf("%s\n", argv[0]);
 	(void)argc;
 	return (0);
 }
