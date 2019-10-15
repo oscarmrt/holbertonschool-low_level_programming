69eb2511b51c8563832f532297f04512dd0aa103
oscarmrt <oscarmrt@hotmail.com>
2019-10-15 23:38:23 +0000

    file 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *  *_strdup - Entry point
+ *
+ * Description: write a function that returns a pointer to a newly allocated
+ * space in memory, which contains a copy of the string given as a parameter
+ * @str: Is the parameter to be checked
+ * Return: Always 0 (Success)
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int q;
+	int w;
+
+	if (str == 0)
+		return (NULL);
+	for (w = 0; str[w] != 0; w++)
+	{
+	}
+
+	s = malloc(w * (1 + sizeof(char)));
+	if (s == NULL)
+		return (NULL);
+
+	for (q = 0; q < w; q++)
+		s[q] = str[q];
+	return (s);
+}
