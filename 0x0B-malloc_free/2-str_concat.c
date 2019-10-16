7c843901a41c5ecdb13202591fb19e0e44c0aa57
oscarmrt <oscarmrt@hotmail.com>
2019-10-16 00:19:36 +0000

    file 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *  *str_concat - Entry point
+ *
+ * Description: write a function that concatenates two strings
+ * @s1: Is the parameter to be checked
+ * @s2: Is the parameter to be checked
+ * Return: Always 0 (Success)
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *s;
+	int q;
+	int w;
+	int r;
+	int f;
+
+	for (q = 0; s1[q] != '\0'; q++)
+	{
+	}
+	for (w = 0; s2[w] != '\0'; w++)
+	{
+	}
+	s = malloc((sizeof(char) * q) + (sizeof(char) * (w + 1)));
+	if (s == NULL)
+		return (NULL);
+
+	for (r = 0; r < q; r++)
+		s[r] = s1[r];
+	for (f = 0; f < w; f++, r++)
+		s[r] = s2[f];
+	s[r] = '\0';
+	return (s);
+}
