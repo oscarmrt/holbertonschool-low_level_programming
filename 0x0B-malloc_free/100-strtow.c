cb3905285e90b3108a18945416bb7bfb46d430ef
oscarmrt <oscarmrt@hotmail.com>
2019-10-16 02:21:19 +0000

    file 5 and 100

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * **strtow - Entry point
+ *
+ * Description: write a function that splits a string into words
+ * created by your alloc_grid function
+ * @str: Is the parameter to be checked
+ * Return: Always 0 (Success)
+ */
+char **strtow(char *str)
+{
+	int k;
+
+	if (str == NULL)
+		return (NULL);
+	if (str == "")
+		return (NULL);
+
+	for (k = 0; str[k] != '\0'; k++)
+	{
+	}
+}
---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *argstostr - Entry point
+ *
+ * Description: write a function that concatenates all the arguments
+ * of your program
+ * @ac: Is the parameter to be checked
+ * @av: Is the parameter to be checked
+ * Return: Always 0 (Success)
+ */
+char *argstostr(int ac, char **av)
+{
+	int k;
+
+	if (ac == 0)
+		return (NULL);
+	if (av == NULL)
+		return (NULL);
+
+}
