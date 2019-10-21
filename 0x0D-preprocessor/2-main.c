082e49d019de161c42c57f319fbf40e9c8aa2639
oscarmrt <oscarmrt@hotmail.com>
2019-10-21 21:57:34 +0000

    modified file 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Description: write a program that prints the name of the file it was
+ * compiled from, followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf(__FILE__"\n");
