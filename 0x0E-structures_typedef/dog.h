2f3010b94028ff27f2b7cdeb52833a2ede5d37f8
oscarmrt <oscarmrt@hotmail.com>
2019-10-21 23:13:55 +0000

    new directory and file 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_0_
+#define _DOG_0_
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+int _putchar(char c);
+
+#endif /* #ifndef _DOG_0_*/
