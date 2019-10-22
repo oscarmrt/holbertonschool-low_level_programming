0007732a9c5d4ed65a702513b09ed00dd163cc5b
oscarmrt <oscarmrt@hotmail.com>
2019-10-22 01:16:11 +0000

    modified file 4 and 5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 #include"dog.h"
+#include <stdlib.h>
 /**
- *  new_dog - Short description
+ * *new_dog - Short description
  * @name: First member
  * @age: Second member
  * @owner: Third member
  *
  * Description: Longer description
+ * Return: new_dog
  */
-dog_t *new_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 
