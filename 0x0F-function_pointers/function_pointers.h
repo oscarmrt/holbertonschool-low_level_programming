3da09a2e0d1aa00be748307e6200040283d2f360
oscarmrt <oscarmrt@hotmail.com>
2019-10-24 15:31:35 +0000

    modified header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
 #ifndef _FUNCTION_POINTERS_
 #define _FUNCTION_POINTERS_
-#define <stdio.h>
-#define <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
