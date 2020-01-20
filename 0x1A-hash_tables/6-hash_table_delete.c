29bf0cbd9ae2d397706105f7cdcb9f4ad6973535
oscarmrt <oscarmrt@hotmail.com>
2020-01-20 03:27:21 +0000

    file 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
 /**
- * hash_table_print - Entry point
+ * hash_table_delete - Entry point
  *
  * Description: Write a function that prints a hash table.
  * @ht: Is the parameter to be checked.
@@ -9,5 +9,23 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    
+	unsigned long int hash_index;
+	hash_node_t *opt;
+
+	if (ht == NULL || ht->array == NULL || ht->size == 0)
+		return;
+	for (hash_index = 0; hash_index < ht->size; hash_index++)
+	{
+		for (; ht->array[hash_index] != NULL; ht->array[hash_index] = opt)
+		{
+			opt = ht->array[hash_index]->next;
+			free(ht->array[hash_index]->key);
+			free(ht->array[hash_index]->value);
+			free(ht->array[hash_index]);
+		}
+	}
+	free(ht->array);
+	ht->array = NULL;
+	ht->size = 0;
+	free(ht);
 }
