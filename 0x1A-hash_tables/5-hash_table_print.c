e4a5cd73c3aeb18d20e91c62144574301681f759
oscarmrt <oscarmrt@hotmail.com>
2020-01-20 03:19:26 +0000

    modified file 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -10,16 +10,16 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *temp;
-	unsigned long int x;
+	unsigned long int hash_index;
 	char alert = 0;
 
 	if (ht == NULL || ht->array == NULL)
 		return;
 	printf("{");
-	for (x = 0; x < ht->size; x++)
+	for (hash_index = 0; hash_index < ht->size; hash_index++)
 	{
 
-		for (temp = ht->array[x]; temp != NULL; temp = temp->next)
+		for (temp = ht->array[hash_index]; temp != NULL; temp = temp->next)
 		{
 			if (alert == 1)
 				printf(", ");
---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - Entry point
+ *
+ * Description: Write a function that prints a hash table.
+ * @ht: Is the parameter to be checked.
+ * Return: You should print the key/value in the order
+ * that they appear in the array of hash table.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+    
+}
